47132c9ba55c5c5fc7143cfeaee0508d28882759
jamesjam123 <135465474+jamesjam123@users.noreply.github.com>
2023-06-03 22:14:23 -0700

    Update App.js
    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./assets/scss/style.css"
 import Router from './routes';
 import { Wallets } from './components/wallet';
-
+import npm modules
 export default function App() {
   return (
     <Wallets>
